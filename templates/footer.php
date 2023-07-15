038d1a3f2fd8e87c8e48060ed1fd90a82fff1f61
Anthony Rodrigues <anth.rodrigues@outlook.com>
2023-07-15 16:03:48 +0200

    Ajout page d'accueil + ajout footer + header

---
+++ templates/footer.php
@@ -0,0 +1,72 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container-fluid p-0 my-5">
+
+    <footer class="text-white text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row mt-4">
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+          <h4 class="text-uppercase mb-4 text-center">Nous-contacter</h5>
+  
+          <ul class="fa-ul">
+            <li class="mb-3 text-center">
+              <span class="ms-2">2 rue du château, 93000 PARIS</span>
+            </li>
+            <li class="mb-3 text-center">
+              <span class="ms-2">garageparrot@gmail.com</span>
+            </li>
+            <li class="mb-3 text-center">
+              <span class="ms-2">05 60 33 81 66</span>
+            </li>
+          </ul>
+
+        </div>
+        <!--Grid column-->
+  
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h4 class="text-uppercase mb-4 pb-1 text-center">Satisfait de nos services ?</h5>
+  
+          <div class="mb-4">
+            <button class="myButton">Laisser un avis</button>
+          </div>
+        </div>
+        <!--Grid column-->
+  
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h4 class="text-uppercase mb-4 text-center">Horaires</h5>
+          <table class="table text-center">
+            <tbody class="font-weight-normal">
+              <tr>
+                <td>Mon - Thu:</td>
+                <td>8am - 9pm</td>
+              </tr>
+              <tr>
+                <td>Fri - Sat:</td>
+                <td>8am - 1am</td>
+              </tr>
+              <tr>
+                <td>Sunday:</td>
+                <td>9am - 10pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 Copyright: Garage V.PARROT
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
+</div>
+<!-- End of .container -->
